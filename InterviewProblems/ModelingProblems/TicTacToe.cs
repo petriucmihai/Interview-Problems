3a1c2c70991d0b6364ee6752664f21889b085cc1
petriucmihai <mihaitp10@gmail.com>
2019-10-04 15:50:09 -0700

    Added problem description to TicTacToe

--- InterviewProblems/ModelingProblems/TicTacToe.cs
+++ InterviewProblems/ModelingProblems/TicTacToe.cs
@@ -8,6 +8,16 @@ namespace InterviewProblems.ModelingProblems
 {
     class TicTacToe
     {
+        /*
+            Design a Tic-tac-toe game that is played between two players on a n x n grid.
+
+            You may assume the following rules:
+
+            A move is guaranteed to be valid and is placed on an empty block.
+            Once a winning condition is reached, no more moves is allowed.
+            A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
+        */
+
         private int[,] board;
 
         /** Initialize your data structure here. */
