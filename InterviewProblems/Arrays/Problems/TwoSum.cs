b2da4a8895ae4a336b124d0030c35acf7790326d
petriucmihai <mihaitp10@gmail.com>
2019-10-04 11:46:17 -0700

    Add description to TwoSum

--- InterviewProblems/Arrays/Problems/TwoSum.cs
+++ InterviewProblems/Arrays/Problems/TwoSum.cs
@@ -8,6 +8,20 @@ namespace InterviewProblems.Arrays.Problems
 {
     class TwoSum
     {
+        /*
+         *  Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+            You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+            Example:
+
+            Given nums = [2, 7, 11, 15], target = 9,
+
+            Because nums[0] + nums[1] = 2 + 7 = 9,
+            return [0, 1].
+        */
+
+
         // Time: O(n^2), Space: O(1)
         public static int[] TwoSumBruteForce(int[] nums, int target)
         {
