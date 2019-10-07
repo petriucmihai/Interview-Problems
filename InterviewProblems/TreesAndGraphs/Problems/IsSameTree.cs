fe1aec4ab365947e1346bdf994129ab82f3adb5b
petriucmihai <mihaitp10@gmail.com>
2019-10-07 14:40:38 -0700

    Added IsSameTree problem

---
+++ InterviewProblems/TreesAndGraphs/Problems/IsSameTree.cs
@@ -0,0 +1,26 @@
+﻿using InterviewProblems.TreesAndGraphs.UtilityClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProblems.TreesAndGraphs.Problems
+{
+    class IsSameTree
+    {
+        public static bool IsSameTreeRecursive(TreeNode p, TreeNode q)
+        {
+            if (p == null & q == null)
+                return true;
+
+            if (p == null || q == null)
+                return false;
+
+            if (p.val != q.val)
+                return false;
+
+            return IsSameTreeRecursive(p.right, q.right) && IsSameTreeRecursive(p.left, q.left);
+        }
+    }
+}
