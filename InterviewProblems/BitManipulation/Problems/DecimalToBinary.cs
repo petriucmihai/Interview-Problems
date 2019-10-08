345ad5c0e52d42cc64cbc424d01c64be47d69d31
petriucmihai <mihaitp10@gmail.com>
2019-10-07 17:32:52 -0700

    Added decimal to binary class, with starting method for printing binary representation of decimal number between 0 and 1

---
+++ InterviewProblems/BitManipulation/Problems/DecimalToBinary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProblems.BitManipulation.Problems
+{
+    class DecimalToBinary
+    {
+        public static string PrintBinaryBetween0And1(double num)
+        {
+            if (num >= 1 || num <= 0)
+            {
+                return "ERROR";
+            }
+
+            StringBuilder binaryResult = new StringBuilder();
+            binaryResult.Append(".");
+            while (num > 0)
+            {
+                if (binaryResult.Length >= 32)
+                {
+                    return "ERROR";
+                }
+
+                double current = num * 2;
+                if (current >= 1)
+                {
+                    binaryResult.Append("1");
+                    num = current - 1;
+                }
+                else
+                {
+                    binaryResult.Append(0);
+                    num = current;
+                }
+            }
+
+            return binaryResult.ToString();
+        }
+    }
+}
