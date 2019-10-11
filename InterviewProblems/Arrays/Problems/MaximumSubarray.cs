7aeb8b2772240991e1d6b2c786f089117bc21cf3
petriucmihai <mihaitp10@gmail.com>
2019-10-10 20:07:19 -0700

    Added Maximum subarray problem, unfinished

---
+++ InterviewProblems/Arrays/Problems/MaximumSubarray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProblems.Arrays.Problems
+{
+    class MaximumSubarray
+    {
+        /*
+            Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+                Example:
+
+                Input: [-2,1,-3,4,-1,2,1,-5,4],
+                Output: 6
+                Explanation: [4,-1,2,1] has the largest sum = 6.
+        */
+
+        public static int MaxSubArray(int[] nums)
+        {
+            return 0;
+        }
+    }
+}
