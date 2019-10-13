f0e258ef42141a635f6ae9850b54b4f153be9e8b
petriucmihai <mihaitp10@gmail.com>
2019-10-12 19:18:59 -0700

    Added answer to spiral matrix

--- InterviewProblems/Arrays/Problems/SpiralMatrix.cs
+++ InterviewProblems/Arrays/Problems/SpiralMatrix.cs
@@ -32,5 +32,54 @@ namespace InterviewProblems.Arrays.Problems
         ]
         Output: [1,2,3,4,8,12,11,10,9,5,6,7]
         */
+
+        public static IList<int> SpiralOrder(int[][] matrix)
+        {
+            IList<int> result = new List<int>();
+
+            if (matrix == null || matrix.Length == 0)
+                return result;
+
+            int endRow = matrix.Length;
+            int endCol = matrix[0].Length;
+
+            int startRow = 0;
+            int startCol = 0;
+            int i = 0;
+
+            while (startRow < endRow && startCol < endCol)
+            {
+                for (i = startCol; i < endCol; i++)
+                {
+                    result.Add(matrix[startRow][i]);
+                }
+                startRow++;
+
+                for (i = startRow; i < endRow; i++)
+                {
+                    result.Add(matrix[i][endCol - 1]);
+                }
+                endCol--;
+
+                if (startRow < endRow)
+                {
+                    for (i = endCol - 1; i >= startCol; i--)
+                    {
+                        result.Add(matrix[endRow - 1][i]);
+                    }
+                    endRow--;
+                }
+
+                if (startCol < endCol)
+                {
+                    for (i = endRow - 1; i >= startRow; i--)
+                    {
+                        result.Add(matrix[i][startCol]);
+                    }
+                    startCol++;
+                }
+            }
+            return result;
+        }
     }
 }
