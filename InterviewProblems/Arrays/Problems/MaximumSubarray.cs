a8aa3a2e5ad5fe674521f780f68ea2dbff45b129
petriucmihai <mihaitp10@gmail.com>
2019-10-16 20:33:21 -0700

    Added code for maximum subarray

--- InterviewProblems/Arrays/Problems/MaximumSubarray.cs
+++ InterviewProblems/Arrays/Problems/MaximumSubarray.cs
@@ -20,7 +20,45 @@ namespace InterviewProblems.Arrays.Problems
 
         public static int MaxSubArray(int[] nums)
         {
-            return 0;
+            return MaxSubArray(nums, 0, nums.Length - 1);
+        }
+
+        private static int MaxSubArray(int[] nums, int left, int right)
+        {
+            if (left == right)
+                return nums[left];
+
+            int mid = (left + right) / 2;
+
+            int leftSum = MaxSubArray(nums, left, mid);
+            int rightSum = MaxSubArray(nums, mid + 1, right);
+            int crossSum = CrossSum(nums, left, right, mid);
+
+            return Math.Max(Math.Max(leftSum, rightSum), crossSum);
+        }
+
+        private static int CrossSum(int[] nums, int left, int right, int mid)
+        {
+            if (left == right)
+                return nums[left];
+
+            int leftSubSum = Int32.MinValue;
+            int currSum = 0;
+            for (int i = mid; i >= left; i--)
+            {
+                currSum += nums[i];
+                leftSubSum = Math.Max(leftSubSum, currSum);
+            }
+
+            int rightSubSum = Int32.MinValue;
+            currSum = 0;
+            for (int i = mid + 1; i < right + 1; i++)
+            {
+                currSum += nums[i];
+                rightSubSum = Math.Max(rightSubSum, currSum);
+            }
+
+            return leftSubSum + rightSubSum;
         }
     }
 }
