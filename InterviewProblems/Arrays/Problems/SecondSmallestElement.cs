cfa49d895dda673621d77427dae575550f367c16
petriucmihai <mihaitp10@gmail.com>
2019-10-21 16:29:50 -0700

    Added second smallest element of array

---
+++ InterviewProblems/Arrays/Problems/SecondSmallestElement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProblems.Arrays.Problems
+{
+    class SecondSmallestElement
+    {
+        // Find the second smallest element in an array
+        public static int FindSecondSmallest(int[] array)
+        {
+            if (array.Length < 2)
+            {
+                throw new ArgumentException();
+            }
+            
+            int min = Int32.MaxValue;
+            int second = Int32.MaxValue;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                // if item is min, update min and second
+                if (array[i] < min)
+                {
+                    second = min;
+                    min = array[i];
+                }
+                // if item is greater than min but less than second
+                else if (array[i] < second && array[i] != min)
+                {
+                    second = array[i];
+                }
+            }
+
+            return second;
+        }
+    }
+}
