0859835d9da39ecd936bad3558b14bf486f23c2f
petriucmihai <mihaitp10@gmail.com>
2019-10-21 19:47:53 -0700

    Added sort a stack

---
+++ InterviewProblems/StacksAndQueues/Problems/SortAStack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProblems.StacksAndQueues.Problems
+{
+    class SortAStack
+    {
+        // Given a stack of integers, sort it in ascending order
+
+        public static void SortStackWithTempStack(Stack<int> input)
+        {
+            Stack<int> tempStack = new Stack<int>();
+            while (input.Count > 0)
+            {
+                int num = input.Pop();
+                while (tempStack.Count > 0 && tempStack.Peek() > num)
+                {
+                    input.Push(tempStack.Pop());
+                }
+                tempStack.Push(num);
+            }
+            input = tempStack;
+        }
+
+
+    }
+}
