9d00ac8bc19c112f3e0e2fcb7cb674a8487ca75f
petriucmihai <mihaitp10@gmail.com>
2019-10-21 18:26:45 -0700

    Added first non repeating element

---
+++ InterviewProblems/Arrays/Problems/FirstNonRepeatingElement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProblems.Arrays.Problems
+{
+    class FirstNonRepeatingElement
+    {
+        // Find the first non-repeating element in a given array of integers
+        public static int FindFirstNonRepeating(int[] array)
+        {
+            Dictionary<int, int> elementCounts = new Dictionary<int, int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (!elementCounts.ContainsKey(array[i]))
+                {
+                    elementCounts.Add(array[i], 1);
+                }
+                else
+                {
+                    elementCounts[array[i]]++;
+                }
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (elementCounts[array[i]] == 1)
+                    return array[i];
+            }
+
+            return -1;
+        }
+    }
+}
