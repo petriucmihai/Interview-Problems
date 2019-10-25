496255d724fea67bd4ab1d4df487e59ff73af339
petriucmihai <mihaitp10@gmail.com>
2019-10-25 13:55:00 -0700

    Added binary search, LCA, and K closest points to origin

---
+++ InterviewProblems/SearchingAndSorting/SearchAlgorithms/BinarySearch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProblems.SearchingAndSorting.SearchAlgorithms
+{
+    class BinarySearch
+    {
+        public static int Search(int[] arr, int target)
+        {
+            return Search(arr, target, 0, arr.Length - 1);
+        }
+
+        private static int Search(int[] arr, int target, int left, int right)
+        {
+            if (right >= left)
+            {
+                int mid = (right + left) / 2;
+
+                if (arr[mid] == target)
+                {
+                    return mid;
+                }
+                else if (arr[mid] > target)
+                {
+                    return Search(arr, target, left, mid - 1);
+                }
+                else
+                {
+                    return Search(arr, target, mid + 1, right);
+                }
+            }
+            return -1;
+        }
+    }
+}
--- InterviewProblems/TreesAndGraphs/Problems/LowestCommonAncestor.cs
+++ InterviewProblems/TreesAndGraphs/Problems/LowestCommonAncestor.cs
@@ -9,12 +9,12 @@ namespace InterviewProblems.TreesAndGraphs.Problems
 {
     class LowestCommonAncestor
     {
-        public static TreeNode LowestCommonAncestorOfTree(TreeNode root, TreeNode p, TreeNode q)
+        public static TreeNode LowestCommonAncestorOfBST(TreeNode root, TreeNode p, TreeNode q)
         {
-            return LCA(root, p, q);
+            return LCABST(root, p, q);
         }
 
-        private static TreeNode LCA(TreeNode node, TreeNode p, TreeNode q)
+        private static TreeNode LCABST(TreeNode node, TreeNode p, TreeNode q)
         {
 
             if (node == null)
@@ -25,17 +25,56 @@ namespace InterviewProblems.TreesAndGraphs.Problems
             // If both n1 and n2 are smaller than root, then LCA lies in left  
             if (node.val > p.val && node.val > q.val)
             {
-                return LCA(node.left, p, q);
+                return LCABST(node.left, p, q);
             }
 
             // If both n1 and n2 are greater than root, then LCA lies in right  
             if (node.val < p.val && node.val < q.val)
             {
-                return LCA(node.right, p, q);
+                return LCABST(node.right, p, q);
             }
 
             return node;
 
         }
+
+        public static TreeNode LowestCommonAncestorOfTreeLCAConfirmed(TreeNode node, TreeNode p, TreeNode q)
+        {
+            // Base case 
+            if (node == null)
+                return null;
+
+            // If either n1 or n2 matches with root's key, report 
+            // the presence by returning root (Note that if a key is 
+            // ancestor of other, then the ancestor key becomes LCA 
+            if (node == p || node == q)
+                return node;
+
+            // Look for keys in left and right subtrees 
+            TreeNode leftLCA = LowestCommonAncestorOfTreeLCAConfirmed(node.left, p, q);
+            TreeNode rightLCA = LowestCommonAncestorOfTreeLCAConfirmed(node.right, p, q);
+
+            // If both of the above calls return Non-NULL, then one key 
+            // is present in once subtree and other is present in other, 
+            // So this node is the LCA 
+            if (leftLCA != null && rightLCA != null)
+            {
+                return node;
+            }
+            else if (leftLCA != null)
+            {
+                return leftLCA;
+            }
+            else if (rightLCA != null)
+            {
+                return rightLCA;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        
     }
 }
