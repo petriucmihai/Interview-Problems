ba8ae16caa752b75e7ec5239320e781a67875a12
Mihai Petriuc <mihaitp10@gmail.com>
2019-12-09 14:26:54 -0800

    Added removal of duplicates from sorted array, in-place

--- InterviewProblems/LinkedLists/Problems/RemoveDuplicates.cs
+++ InterviewProblems/LinkedLists/Problems/RemoveDuplicates.cs
@@ -61,5 +61,58 @@ namespace InterviewProblems.LinkedLists.Problems
                 }
             }
         }
+
+        /*
+        Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+        Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+        Example 1:
+
+        Given nums = [1,1,2],
+
+        Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+        It doesn't matter what you leave beyond the returned length.
+        Example 2:
+
+        Given nums = [0,0,1,1,1,2,2,3,3,4],
+
+        Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+
+        It doesn't matter what values are set beyond the returned length.
+        */
+        public static int RemoveDuplicatesFromSortedArray(int[] nums)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                while (i < nums.Length - 1 && nums[i + 1] == nums[i])
+                {
+                    nums[i] = Int32.MaxValue;
+                    i++;
+                }
+                count++;
+            }
+
+            int swapIndex = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] < Int32.MaxValue)
+                {
+                    Swap(nums, swapIndex, i);
+                    swapIndex++;
+                }
+            }
+
+            return count;
+        }
+
+        private static void Swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
     }
 }
