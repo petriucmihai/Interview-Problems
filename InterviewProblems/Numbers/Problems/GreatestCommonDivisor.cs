3eac3548309ed49c61ec15932c3874aa128c44f1
Mihai Petriuc <mihaitp10@gmail.com>
2019-12-13 10:50:10 -0800

    Added greatest common divisor

---
+++ InterviewProblems/Numbers/Problems/GreatestCommonDivisor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProblems.Numbers.Problems
+{
+    class GreatestCommonDivisor
+    {
+        public static int GCD(int a, int b)
+        {
+            if (a == 0)
+                return b;
+
+            return GCD(b % a, a);
+        }
+
+        static int MultipleValueGCD(int[] arr, int n)
+        {
+            int result = arr[0];
+            for (int i = 1; i < n; i++)
+            {
+                result = GCD(arr[i], result);
+
+                if (result == 1)
+                {
+                    return 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
