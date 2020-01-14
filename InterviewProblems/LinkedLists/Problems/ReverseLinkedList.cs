ab279c34ca4b34ef457fab5f16f4893750681ed1
Mihai Petriuc <mihaitp10@gmail.com>
2020-01-13 19:46:18 -0800

    Added ReverseBetween to reverse linked list

--- InterviewProblems/LinkedLists/Problems/ReverseLinkedList.cs
+++ InterviewProblems/LinkedLists/Problems/ReverseLinkedList.cs
@@ -33,5 +33,52 @@ namespace InterviewProblems.LinkedLists.Problems
 
             return prev;
         }
+
+        public Node ReverseBetween(Node head, int m, int n)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            Node curr = head;
+            Node prev = null;
+            while (m > 1)
+            {
+                prev = curr;
+                curr = curr.next;
+                m--;
+                n--;
+            }
+
+            // The two pointers that will fix the final connections.
+            Node con = prev;
+            Node tail = curr;
+
+            // Iteratively reverse the nodes until n becomes 0.
+            Node third = null;
+            while (n > 0)
+            {
+                third = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = third;
+                n--;
+            }
+
+            // Adjust the final connections
+            if (con != null)
+            {
+                con.next = prev;
+            }
+            else
+            {
+                head = prev;
+            }
+
+            tail.next = curr;
+            return head;
+
+        }
     }
 }
