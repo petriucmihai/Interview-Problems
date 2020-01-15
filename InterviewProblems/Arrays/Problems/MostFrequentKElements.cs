6f4f8807bd43152cb6ad0d83f1b2420d944ca12e
Mihai Petriuc <mihaitp10@gmail.com>
2020-01-14 18:44:25 -0800

    Added K most frequent elements

---
+++ InterviewProblems/Arrays/Problems/MostFrequentKElements.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProblems.Arrays.Problems
+{
+    class MostFrequentKElements
+    {
+        /*
+        Given a non-empty array of integers, return the k most frequent elements.
+
+        Example 1:
+
+        Input: nums = [1,1,1,2,2,3], k = 2
+        Output: [1,2]
+        Example 2:
+
+        Input: nums = [1], k = 1
+        Output: [1]
+        */
+        public IList<int> TopKFrequent(int[] nums, int k)
+        {
+            if (nums.Length == 0 || nums == null)
+            {
+                return null;
+            }
+
+            Dictionary<int, int> numOccur = new Dictionary<int, int>();
+
+            foreach (int num in nums)
+            {
+                if (numOccur.ContainsKey(num))
+                {
+                    numOccur[num]++;
+                }
+                else
+                {
+                    numOccur.Add(num, 1);
+                }
+            }
+
+            int key = 0;
+            int keyCount = 0;
+            int[] result = new int[k];
+
+            for (int i = 0; i < k; i++)
+            {
+                foreach (int num in numOccur.Keys)
+                {
+                    if (numOccur[num] > keyCount)
+                    {
+                        keyCount = numOccur[num];
+                        key = num;
+                    }
+                }
+                numOccur.Remove(key);
+                result[i] = key;
+                key = 0;
+                keyCount = 0;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ InterviewProblems/Strings/Problems/LongestPalindromicSubstring.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewProblems.Strings.Problems
+{
+    class LongestPalindromicSubstring
+    {
+        /*
+        Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+        Example 1:
+
+        Input: "babad"
+        Output: "bab"
+        Note: "aba" is also a valid answer.
+        Example 2:
+
+        Input: "cbbd"
+        Output: "bb"
+        */
+        public string LongestPalindrome(string s)
+        {
+            int longest = 0;
+            int startIndex = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int left = i;
+                int right = i;
+
+                while (left >= 0 && s[left] == s[i])
+                {
+                    left--;
+                }
+                while (right < s.Length && s[right] == s[i])
+                {
+                    right++;
+                }
+
+                while (left >= 0 && right < s.Length && s[right] == s[left])
+                {
+                    left--;
+                    right++;
+                }
+
+                int currentLength = right - left - 1;
+
+                if (longest < currentLength)
+                {
+                    longest = currentLength;
+                    startIndex = left + 1;
+                }
+            }
+
+            return s.Substring(startIndex, longest);
+        }
+    }
+}
